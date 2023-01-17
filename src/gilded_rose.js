438dfdd77ac4a8fb988cd2f364eedfd6949bbc7c
Sneha Patil <snehampatil29@gmail.com>
2023-01-17 12:55:20 -0500

    refactor-kata complete

---
+++ src/gilded_rose.js
@@ -0,0 +1,69 @@
+class Item {
+  constructor(name, sellIn, quality) {
+    this.name = name
+    this.sellIn = sellIn
+    this.quality = quality
+  }
+}
+
+const calculateQualityDiffForNormalItem = ({ sellIn, quality }) => {
+  const isQualityGreaterThanZero = quality > 0
+  const cannotSell = sellIn < 0
+
+  if (isQualityGreaterThanZero && cannotSell) return -2
+  if (isQualityGreaterThanZero) return -1
+  return 0
+}
+
+const calculateQualityDiffForBackstage = ({ sellIn, quality }) => {
+  const tenDaysOrLessToSell = sellIn <= 10
+  const fiveDaysOrLessToSell = sellIn <= 5
+  const cannotSell = sellIn < 0
+
+  if (cannotSell) return -quality
+  if (fiveDaysOrLessToSell) return +3
+  if (tenDaysOrLessToSell) return +2
+
+  return +1
+}
+
+const calculateSellinDifference = ({ name }) => {
+  const isSulfuras = name == 'Sulfuras, Hand of Ragnaros'
+  return !isSulfuras ? -1 : 0
+}
+
+const calculateQualityDifference = (item) => {
+  const isSulfuras = item.name == 'Sulfuras, Hand of Ragnaros'
+  const isAgedBrie = item.name == 'Aged Brie'
+  const isConjuredItem = item.name.includes('Conjured')
+  const isBackstagePasses =
+    item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  const isQualityLessThan50 = item.quality < 50
+  const isNormalItem =
+    !isAgedBrie && !isBackstagePasses && !isSulfuras && !isConjuredItem
+
+  if (isConjuredItem) return calculateQualityDiffForNormalItem(item) * 2
+  if (isNormalItem) return calculateQualityDiffForNormalItem(item)
+  if (isBackstagePasses) return calculateQualityDiffForBackstage(item)
+  if (isAgedBrie && isQualityLessThan50) return +1
+  return 0
+}
+
+class Shop {
+  constructor(items = []) {
+    this.items = items
+  }
+
+  updateQuality() {
+    return this.items.map((item) => {
+      item.sellIn += calculateSellinDifference(item)
+      item.quality += calculateQualityDifference(item)
+      return item
+    })
+  }
+}
+
+module.exports = {
+  Item,
+  Shop,
+}
